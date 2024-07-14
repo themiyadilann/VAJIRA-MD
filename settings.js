0308410ff32d9f510f4badbc4c622773ed8c8318
themiyadilann <128908885+themiyadilann@users.noreply.github.com>
2024-07-14 10:34:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=2I1nBZaL#NYjv1ykeAXCTEU_NsntZH3Dkyz0112vkK9ZqCKWw1xI' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=zR9nWLIJ#Ae4B-2_brEb55UoBnqmUBEqB2RNSaFW5A2KqmfIkm9E' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
